2bd82af2d1af570577961be6b171f7727093852a
jur <jelle@samson-it.nl>
2015-08-06 14:26:07 +0200

    removed release parameters.yml support from the bundle now that we have the appkernel parameters.yml symlink convention

--- Command/PrepareReleaseCommand.php
+++ Command/PrepareReleaseCommand.php
@@ -8,8 +8,6 @@ use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
 use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
-use Symfony\Component\Finder\Finder;
-use Symfony\Component\Finder\SplFileInfo;
 
 /**
  * @author Bart van den Burg <bart@samson-it.nl>
@@ -22,9 +20,7 @@ class PrepareReleaseCommand extends ContainerAwareCommand
         $this
             ->setName('samson:release:prepare')->setAliases(array('samson:preparerelease'))
             ->addArgument('appname', InputArgument::REQUIRED, 'The name of your application')
-            ->addArgument('instance', \Symfony\Component\Console\Input\InputArgument::OPTIONAL, 'The parameters.yml file to use')
             ->addOption('target', 't', InputOption::VALUE_OPTIONAL, 'Where should we put the prepared source?')
-            ->addOption('skip-vendors', null, InputOption::VALUE_NONE, 'Don\'t include vendors')
             ->addOption('skip-vendors', null, InputOption::VALUE_NONE, 'Don\'t include vendors')
             ->addOption('force', 'f', InputOption::VALUE_NONE, 'Ignore git errors');
     }
@@ -38,32 +34,6 @@ class PrepareReleaseCommand extends ContainerAwareCommand
 
     protected function execute(InputInterface $input, OutputInterface $output)
     {
-        $instance = $input->getArgument('instance', null);
-        if (null === $instance) {
-            $finder = Finder::create()
-                ->in($this->getContainer()->getParameter('kernel.root_dir') . '/config/instance')
-                ->name('/.*?\.yml/');
-
-            if (count($finder)) {
-                $instances = array_values(array_map(function (SplFileInfo $file) {
-                    return $file->getFilename();
-                }, iterator_to_array($finder)));
-                array_unshift($instances, 'no');
-
-                /* @var $helper \Symfony\Component\Console\Helper\DialogHelper */
-                $helper = $this->getHelper('dialog');
-                $answer = $helper->select($output, 'Include an instance parameters.yml file? (default: no)', $instances, 0);
-
-                if ($answer > 0) {
-                    $instance = $instances[$answer];
-                }
-            }
-
-        }
-
-        if (null === $instance) {
-            $output->writeln('<comment>Warning: no instance file supplied. The package will not contain a parameters.yml file!</comment>');
-        }
         $determiner = $this->getContainer()->get('samson_release.version_determiner');
 
         if (!$input->getOption('force')) {
@@ -97,7 +67,7 @@ class PrepareReleaseCommand extends ContainerAwareCommand
         $prepareRelease->setOutput($output);
         $prepareRelease->preparerelease($this->getContainer()->getParameter('kernel.root_dir') . '/..', $target, $instance);
 
-        $fs = new Filesystem;
+        $fs = new Filesystem();
         $fs->remove($this->getContainer()->getParameter('kernel.root_dir') . '/version.txt');
     }
 }
